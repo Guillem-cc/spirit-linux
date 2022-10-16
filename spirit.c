5afe6aed82c644c90399038e8b44c5c36969af46
Guillem-cc <113304827+Guillem-cc@users.noreply.github.com>
2022-10-16 10:44:50 +0200

    Update spirit.c

--- spirit.c
+++ spirit.c
@@ -382,7 +382,23 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "ERROR: Cannot retrieve device list\n");
 		return -1;
 	}
+@@ -384,7 +384,15 @@ int main(int argc, char** argv) {
+        }
 
+        memset(uuid, '\0', 41);
+-       memcpy(uuid, list[0], 40);
++       if (list[0])
++         {
++           memcpy(uuid, list[0], 40);
++         }
++       else
++         {
++           fprintf(stderr, "ERROR: Cannot retrieve device list\n");
++           return -1;
++         }
+        idevice_device_list_free(list);
+
+        printf("INFO: Opening device\n");
 	memset(uuid, '\0', 41);
 	memcpy(uuid, list[0], 40);
 	idevice_device_list_free(list);
